d70b65c46b03abbfeb564d750fe554692e693214
qpg93 <qing.peng@outlook.fr>
2020-06-11 18:07:12 -0700

    Basic CSS

--- src/eCom/urls.py
+++ src/eCom/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 from profiles import views
 
@@ -22,3 +24,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
