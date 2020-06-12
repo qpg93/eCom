c947604edab7ecad0a1df075ebf21a25954801f3
qpg93 <qing.peng@outlook.fr>
2020-06-12 15:48:35 -0700

    Contact message sending

--- src/contact/views.py
+++ src/contact/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
+from django.conf import settings
 
 from .forms import contactForm
 
@@ -6,7 +8,13 @@ from .forms import contactForm
 def contact(request):
     form = contactForm(request.POST or None)
     if form.is_valid():
-        print(form.cleaned_data['email'])
+        name = form.cleaned_data['name']
+        comment = form.cleaned_data['comment']
+        subject = 'Message from QPG93 PythonAnywhere.com'
+        emailFrom = form.cleaned_data['email']
+        emailTo = [settings.EMAIL_HOST_USER]
+        message = '%s\n%s\n%s' % (comment, emailFrom, name)
+        send_mail(subject, message, emailFrom, emailTo, fail_silently=False)
 
     context = locals()
     template = 'contact.html'
