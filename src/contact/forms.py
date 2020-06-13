2b8d0d68aaba940d31449d8cc16b4eea9f634b4b
qpg93 <qing.peng@outlook.fr>
2020-06-12 19:21:11 -0700

    Customize contact form

--- src/contact/forms.py
+++ src/contact/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class contactForm(forms.Form):
-    name = forms.CharField(required=False, max_length=100, help_text='100 characters max')
+    name = forms.CharField(required=False, max_length=20, help_text='20 characters max')
     email = forms.EmailField(required=True)
     comment = forms.CharField(required=True, widget=forms.Textarea)
